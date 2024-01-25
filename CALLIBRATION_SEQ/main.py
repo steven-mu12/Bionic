eea731675c64ed86f93260287b7cca525538f708
Steven Mu <xsmu0922@gmail.com>
2024-01-25 13:23:43 -0500

    Create main.py

---
+++ CALLIBRATION_SEQ/main.py
@@ -0,0 +1,4 @@
+
+# 1. get a window open for opencv
+# 2. add text instructons: "Place hand in front of camera (also left / right move instructions)" -> "calibration sequence running, don't move" -> calibration sequence complete, closing window
+# 3. based on y of hand, do some stuff 
